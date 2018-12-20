a141177545d9f51a2759d26fa291de5202edcca1
scottdiemer <sdiemer@cfl.rr.com>
2018-12-19 23:38:55 -0500

    added password hashing

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
 
 const app = express()
 
 app.use(bodyParser.json())
 
 const database = {
-  users: [
-    {
+  users: [{
       id: '123',
       name: 'John',
       email: 'john@gmail.com',
@@ -23,7 +23,12 @@ const database = {
       entries: 0,
       joined: new Date()
     }
-  ]
+  ],
+  login: [{
+    id: '987',
+    hash: '',
+    email: 'john@gmail.com'
+  }]
 }
 
 app.get('/', (req, res) => {
@@ -31,7 +36,17 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-  if(req.body.email === database.users[0].email &&
+
+  // Load hash from your password DB.
+  bcrypt.compare('apples', '$2a$10$Z.yd0mO8seCIvMnF5YjoU.Y9TOPdzFEEtg3JkbEe9lOoLhnoH22FW', function(err, res) {
+    console.log('first guess', res)
+  })
+
+  bcrypt.compare("veggies", '$2a$10$Z.yd0mO8seCIvMnF5YjoU.Y9TOPdzFEEtg3JkbEe9lOoLhnoH22FW', function(err, res) {
+    console.log('second guess', res)
+  })
+
+  if (req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password) {
     res.json('success')
   } else {
@@ -40,46 +55,60 @@ app.post('/signin', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  const { email, name, password } = req.body
+  const {
+    email,
+    name,
+    password
+  } = req.body
+  
   database.users.push({
-      id: '125',
-      name: name,
-      email: email,
-      password: password,
-      entries: 0,
-      joined: new Date()
-    })
-  res.json(database.users[database.users.length-1])
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+  res.json(database.users[database.users.length - 1])
 })
 
 app.get('/profile/:id', (req, res) => {
-  const { id } = req.params
+  const {
+    id
+  } = req.params
   let found = false
   database.users.forEach(user => {
-    if(user.id === id) {
+    if (user.id === id) {
       found = true
       return res.json(user)
     }
   })
-  if(!found) {
+  if (!found) {
     res.status(400).json('not found')
   }
 })
 
 app.put('/image', (req, res) => {
-  const { id } = req.body
+  const {
+    id
+  } = req.body
   let found = false
   database.users.forEach(user => {
-    if(user.id === id) {
+    if (user.id === id) {
       found = true
       user.entries++
-      return res.json(user.entries)
+        return res.json(user.entries)
     }
   })
-  if(!found) {
+  if (!found) {
     res.status(400).json('not found')
   }
 })
+
+// bcrypt.hash(password, null, null, function(err, hash) {
+//   // Store hash in your password DB.
+//   console.log(hash)
+// })
 
 app.listen(3000, () => {
   console.log('app is running on port 3000')
