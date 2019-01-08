201b980e55428281a17559e369ab192f07305c02
scottdiemer <sdiemer@cfl.rr.com>
2019-01-07 23:34:47 -0500

    added knex and connected postgres db

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'scott',
+    password : '',
+    database : 'smart-brain'
+  }
+})
+
+db.select('*').from('users').then(data => {
+  console.log(data)
+})
 
 const app = express()
 
@@ -52,15 +67,17 @@ app.post('/register', (req, res) => {
     name,
     password
   } = req.body
-  
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users[database.users.length - 1])
+  db('users')
+    .returning('*')
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date()
+    })
+    .then(user => {
+      res.json(user[0])
+    })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
