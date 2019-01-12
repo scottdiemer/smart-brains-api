4406ee3d5642ed51d4e6aefbb7b84cefdbf2d098
scottdiemer <sdiemer@cfl.rr.com>
2019-01-11 23:17:40 -0500

    added validation

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai') 
+
+const app = new Clarifai.App({
+  apiKey: 'f491ef86824e41e4981c763f47c8a80f'
+})
+
+const handleApiCall = () => (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data)
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (db) => (req, res) => {
   const {id} = req.body
   db('users').where('id', '=', id)
@@ -10,5 +25,6 @@ const handleImage = (db) => (req, res) => {
 }
 
 module.exports = {
-  handleImage: handleImage
+  handleImage,
+  handleApiCall
 }
--- server.js
+++ server.js
@@ -33,12 +33,10 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', signin.handleSignin(db, bcrypt))
-
 app.post('/register', register.handleRegister(db, bcrypt))
-
 app.get('/profile/:id', profile.handleProfileGet(db))
-
 app.put('/image', image.handleImage(db))
+app.post('/imageurl', image.handleApiCall())
 
 app.listen(3000, () => {
   console.log('app is running on port 3000')
