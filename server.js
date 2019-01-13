1359859b6f9cae24da766ac4c467e7fc465baff9
scottdiemer <sdiemer@cfl.rr.com>
2019-01-12 22:46:25 -0500

    updated database connection info

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-convex-25914',
-    user : 'scott',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 })
 
